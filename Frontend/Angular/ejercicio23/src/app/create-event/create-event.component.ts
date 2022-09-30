0e1d255f0673e348cf411d04fb14767f8e386798
Franco Fonzo <francofonzo1@gmail.com>
2022-09-30 03:13:57 -0300

    Add ejercicio25

--- Frontend/Angular/ejercicio23/src/app/create-event/create-event.component.ts
+++ Frontend/Angular/ejercicio23/src/app/create-event/create-event.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { EventService } from '../event.service';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-event',
@@ -11,27 +11,32 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 export class CreateEventComponent implements OnInit {
 
 	formEvent = this.formBuilder.group({
-		name: "",
-		date: "",
-		time: "",
-		location: this.formBuilder.group({
-       address: "",
-       city: "",
-       country: "" 
+    name: ['', Validators.required],
+    date: ['', Validators.required],
+    time: ['', Validators.required],
+    location: this.formBuilder.group({
+      address: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', [Validators.required, Validators.pattern('[A-Z]{2}')]],
     })
-	}); 
+  }); 
 
   constructor(private eventService: EventService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
 
-  cancel() {
+  cancel(): void {
     this.router.navigate(['/events']);
   }
 
-  onSubmit(){
+  onSubmit(): void{
     this.eventService.saveEvent(this.formEvent.value);
     this.router.navigate(["/events"]);
+  }
+
+  isValid(field: string): boolean | undefined {
+    return this.formEvent.get(field)?.invalid &&
+           this.formEvent.get(field)?.touched;
   }
 }
