4b22136c0ebea0e51a8ea36f73d8aeda4a805f4a
Franco Fonzo <francofonzo1@gmail.com>
2022-10-13 20:58:55 -0300

    Add ejercicio47

---
+++ Frontend/Angular/ejercicio47/src/app/movie.service.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed } from '@angular/core/testing';
+
+import { MovieService } from './movie.service';
+import { ActorService } from './actor.service';
+
+describe('MovieService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: MovieService = TestBed.get(MovieService);
+    expect(service).toBeTruthy();
+  });
+
+  it('adding a cast member increases the cast member count by one', () => {
+    const service: MovieService = TestBed.get(MovieService);
+
+    let mockActorService = TestBed.get(ActorService);
+    // agregamos el codigo aqui
+    const spy = spyOn(mockActorService, "canAddMovieToActor").and.returnValue(true);
+
+    service.addCastMember('Sylvester', 'Rambo', mockActorService);
+    expect(service.castMembers.length).toBe(1)    
+  });
+
+});
