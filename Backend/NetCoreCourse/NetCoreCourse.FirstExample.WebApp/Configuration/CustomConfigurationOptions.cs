b6e5ccc6caa17ac6edf3743e07d55fd0e430aa68
Franco Fonzo <francofonzo1@gmail.com>
2022-10-20 11:32:59 -0300

    Add Ejercico 2.1
    - Add new section in appsettings and use in ExampleController

---
+++ Backend/NetCoreCourse/NetCoreCourse.FirstExample.WebApp/Configuration/CustomConfigurationOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCoreCourse.FirstExample.WebApp.Configuration
+{
+    public class CustomConfigurationOptions
+    {
+        public int IntValue { get; set; }
+        public string StringValue { get; set; }
+        public bool BooleaValue { get; set; }
+    }
+}
---
+++ Backend/NetCoreCourse/NetCoreCourse.FirstExample.WebApp/Controllers/ExampleController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NetCoreCourse.FirstExample.WebApp.Configuration;
+
+namespace NetCoreCourse.FirstExample.WebApp.Controllers
+{
+    
+    [Route("api/[controller]")] //Este decorador permite que el Middleware UseRouting pueda encontrar este endpoint.
+    //Mira! Estamos heredando de ControllerBase
+    public class ExampleController : ControllerBase
+    {
+
+        private readonly CustomConfigurationOptions configuration;
+
+        public ExampleController(IOptions<CustomConfigurationOptions> options)
+        {
+            configuration = options?.Value ?? throw new ArgumentNullException("CustomConfigurationOptions was not properly set.");
+        }
+
+        [HttpGet]
+        public IActionResult Hey()
+        {
+            return Ok("Hola Dev!.");
+        }
+
+        [HttpGet("another")]
+        public IActionResult AnotherHey()
+        {
+            return Ok("Hey Este es otra accion de tu controlador.");
+        }
+        
+        [HttpGet("newsection")]
+        public IActionResult NewSection()
+        {
+            return Ok($"{configuration.StringValue}");
+        }
+    }
+}
